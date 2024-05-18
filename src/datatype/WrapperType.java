45365619b6554580b617c9f045f4b308a0a19de7
Yaswanth Raju <yaswanthraju.v@gmail.com>
2024-05-18 20:04:44 +0530

    Wrapper type added

---
+++ src/datatype/WrapperType.java
@@ -0,0 +1,15 @@
+package datatype;
+
+public class WrapperType {
+    public static void main(String[] args) {
+        Integer a=1;
+        int b =1;
+        Integer c= new Integer(1);  //it will refer to new memory
+
+        System.out.println(System.identityHashCode(a)+" == "+System.identityHashCode(b));
+        System.out.println("a = "+a+" b = "+b+" c="+c);
+//        c=1;
+        String result = (a.equals(c)) ? "matched": "not matched";
+        System.out.println("result: "+result);
+    }
+}
